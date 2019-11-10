11e4022d6055bb7608d75030c3a407f5b5b128f6
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-11-10 18:32:34 +0500

    где-то пункт 4

---
+++ progLabThird/WindowsFormsApp2/WindowsFormsApp2/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Node
+    {
+        private int value;
+        private int x;
+        private int y;
+
+        public Node(int value, int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    }
+}

0b2ec83f1c121d5a901c635c88c8ac5adef4f56b
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-11-10 18:49:50 +0500

    закончен пункт 6

---
+++ progLabThird/WindowsFormsApp2/WindowsFormsApp2/MyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2 
+{
+    class MyModel : IModel
+    {
+        public int Count { get { return nodes.Count; } }
+
+        static Random r = new Random();
+        public void AddNode(int value)
+        {
+            nodes.AddFirst(new Node(value, r.Next(10), r.Next(10)));
+        }
+
+        LinkedList<Node> nodes = new LinkedList<Node>();
+        public void RemoveLastNode()
+        {
+            nodes.RemoveLast();
+        }
+    }
+}
